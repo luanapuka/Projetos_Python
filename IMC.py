e563b4ea19b6d0ea02da832658ee3087723d7eb6
Luana Puka <70983104+luanapuka@users.noreply.github.com>
2022-01-12 17:42:35 -0300

    Create IMC.py

---
+++ IMC.py
@@ -0,0 +1,14 @@
+# Aula11
+
+nome = str(input('Olá, qual o seu nome? '))
+altura = float(input('Digite sua altura: '))
+peso = float(input('Digite seu peso: '))
+imc = peso / (altura*altura)
+if imc < 18.5:
+    print('Você está abaixo do peso ideal! Seu IMC é de: {:.1f}' .format(imc))
+elif 18.5 < imc < 25:
+    print('{}, você está no peso ideal! Seu IMC é de: {:.1f}' .format(nome, imc))
+elif 25 < imc < 40:
+    print('{}, você está com sobrepeso! Seu IMC é de: {:.1f}' .format(nome, imc))
+else:
+    print('{}, você está com obesidade morbida! Seu IMC é de: {:.1f}'.format(nome, imc))
