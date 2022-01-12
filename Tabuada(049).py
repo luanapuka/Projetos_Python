e1f7b4da2999c5647a9e63ce89a120fc03f8e333
Luana Puka <70983104+luanapuka@users.noreply.github.com>
2022-01-12 17:45:09 -0300

    .

--- Tabuada(049).py
+++
@@ -1,8 +0,0 @@
-# Aula13
-
-print('=======Desafio 049==========')
-num=int(input('Digite um numero: '))
-for c in range(1,11,1):
-    mult = num * c
-    print('{} x {:2} = {}'.format(num, c, mult))
-print('Fim')
